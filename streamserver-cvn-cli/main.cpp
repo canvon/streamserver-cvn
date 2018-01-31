322e9c3fdf9673e51899fd8b28f55636ccd0758c
Fabian Pietsch <fabian@canvon.de>
2018-01-31 22:34:34 +0100

    cli: Use QCommandLineParser to process command line arguments

--- streamserver-cvn-cli/main.cpp
+++ streamserver-cvn-cli/main.cpp
@@ -1,8 +1,48 @@
+#include <QTextStream>
+#include <QCommandLineParser>
+
 #include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QTextStream out(stdout), errout(stderr);
 
-    return a.exec();
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Media streaming server from MPEG-TS to HTTP clients");
+    parser.addHelpOption();
+    parser.addOptions({
+        { { "l", "listen" }, "Port to listen on for HTTP streaming client connections",
+          "listen_port", "8000" },
+    });
+    parser.addPositionalArgument("input", "Input file name");
+    parser.process(a);
+
+
+    QString listenPortStr = parser.value("listen");
+    bool ok = false;
+    qint16 listenPort = listenPortStr.toUShort(&ok);
+    if (!ok) {
+        errout << a.applicationName() << ": Invalid port number \""
+               << listenPortStr << "\"" << endl;
+        return 2;
+    }
+
+    out << "Listen port number: " << listenPort << endl;
+
+
+    QStringList args = parser.positionalArguments();
+    if (args.length() != 1) {
+        errout << a.applicationName() << ": Invalid number of arguments"
+               << ": Need exactly one positional argument, the input file" << endl;
+        return 2;
+    }
+
+    QString inputFilePath = args.at(0);
+    out << "Input file path: " << inputFilePath << endl;
+
+
+    // FIXME
+    //return a.exec();
+    return 0;
 }
