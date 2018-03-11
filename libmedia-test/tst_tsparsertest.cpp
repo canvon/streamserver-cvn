7b421428df2154b31d809d2c639afbd7f39793ab
Fabian Pietsch <fabian@canvon.de>
2018-03-11 16:43:07 +0100

    libmedia-test: TSParserTest: Add first real test case

--- libmedia-test/tst_tsparsertest.cpp
+++ libmedia-test/tst_tsparsertest.cpp
@@ -1,6 +1,10 @@
 #include <QString>
 #include <QtTest>
 
+#include "tsparser.h"
+
+#include <QTextStream>
+
 class TSParserTest : public QObject
 {
     Q_OBJECT
@@ -18,7 +22,21 @@ TSParserTest::TSParserTest()
 
 void TSParserTest::testCase1()
 {
-    QVERIFY2(true, "Failure");
+    QByteArray res;
+    QTextStream resStream(&res);
+
+    TS::BitStream tsBits(" 0");
+    while (!tsBits.atEnd()) {
+        TS::bslbf1 bit;
+        tsBits >> bit;
+        if (bit)
+            resStream << "1";
+        else
+            resStream << "0";
+    }
+    resStream << flush;
+
+    QCOMPARE(res, QByteArray("00100000") + QByteArray("00110000"));
 }
 
 QTEST_APPLESS_MAIN(TSParserTest)
