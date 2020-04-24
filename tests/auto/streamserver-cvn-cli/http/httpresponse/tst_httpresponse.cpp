b5077da29b8e77a2897b0e0bf67bf3445de235e7
Fabian Pietsch <fabian@canvon.de>
2020-04-24 14:50:39 +0200

    tests: tst_httpresponse: Finish constructing() & getSetCheck()

--- tests/auto/streamserver-cvn-cli/http/httpresponse/tst_httpresponse.cpp
+++ tests/auto/streamserver-cvn-cli/http/httpresponse/tst_httpresponse.cpp
@@ -19,6 +19,13 @@ void TestHTTPResponse::constructing()
     QCOMPARE(resp.httpVersion(), QString("HTTP/1.0"));
     QCOMPARE(static_cast<int>(resp.statusCode()), 200);
     QCOMPARE(resp.statusMsg(), QString("OK"));
+    QCOMPARE(resp.body(), QByteArray());
+
+    HTTP::Response otherResp(HTTP::SC_400_BadRequest, "Bad Request-est", "HTTP/1.1");
+    QCOMPARE(otherResp.httpVersion(), QString("HTTP/1.1"));
+    QCOMPARE(static_cast<int>(otherResp.statusCode()), 400);
+    QCOMPARE(otherResp.statusMsg(), QString("Bad Request-est"));
+    QCOMPARE(otherResp.body(), QByteArray());
 }
 
 void TestHTTPResponse::getSetCheck()
@@ -26,7 +33,13 @@ void TestHTTPResponse::getSetCheck()
     HTTP::Response resp;
     resp.setHttpVersion("HTTP/1.1");
     QCOMPARE(resp.httpVersion(), QString("HTTP/1.1"));
-    // ...
+    resp.setStatusCode(HTTP::SC_404_NotFound);
+    QCOMPARE(static_cast<int>(resp.statusCode()), 404);
+    resp.setStatusMsg("Not Found Here");
+    QCOMPARE(resp.statusMsg(), QString("Not Found Here"));
+    resp.setBody("ABCdef.");
+    QCOMPARE(resp.body(), QByteArray("ABCdef."));
+    // Headers will need a more specific test.
 }
 
 QTEST_APPLESS_MAIN(TestHTTPResponse)
